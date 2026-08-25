cc8ab5e7b1cfc0779be258fc10e2bf07823f4b9a
Edumug <eduardoarnhold301@gmail.com>
2026-08-25 15:27:47 -0300

    Integra criação de contas ao login

--- firebase-easy/login/login.js
+++ firebase-easy/login/login.js
@@ -2,6 +2,7 @@ const loginInput = document.getElementById("login");
 const senhaInput = document.getElementById("senha");
 const btnEntrar = document.getElementById("btnEntrar");
 const btnMostrarSenha = document.getElementById("btnMostrarSenha");
+const btnCriarConta = document.getElementById("btnCriarConta");
 
 let erro = 0;
 
@@ -13,7 +14,6 @@ const logins = [
 
 // Mostrar / ocultar senha
 btnMostrarSenha.addEventListener("click", () => {
-
     if (senhaInput.type === "password") {
         senhaInput.type = "text";
         btnMostrarSenha.src = "../imgs/olho (1).png";
@@ -21,34 +21,30 @@ btnMostrarSenha.addEventListener("click", () => {
         senhaInput.type = "password";
         btnMostrarSenha.src = "../imgs/olho.png";
     }
-
 });
 
 // Entrar
 btnEntrar.addEventListener("click", () => {
-
-    const login = loginInput.value;
+    const login = loginInput.value.trim();
     const senha = senhaInput.value;
 
-    const usuario = logins.find(u => 
-        u[0] === login && u[1] === senha
-    );
+    const contasCriadas = JSON.parse(localStorage.getItem("contasBoletim") || "[]");
+
+    const usuario = logins.find(u => u[0] === login && u[1] === senha) ||
+        contasCriadas.find(u => u.login === login && u.senha === senha);
 
     if (usuario) {
-    sessionStorage.setItem("logado", "true");
-    window.location.replace("../main/main.html");
-}
- else {
-
+        sessionStorage.setItem("logado", "true");
+        window.location.replace("../main/main.html");
+    } else {
         erro++;
 
         alert(
-            "Login ou senha incorretos! Tentativas restantes: " 
-            + (3 - erro)
+            "Login ou senha incorretos! Tentativas restantes: " +
+            (3 - erro)
         );
 
         if (erro >= 3) {
-
             btnEntrar.disabled = true;
 
             const recuperacao = prompt(
@@ -57,20 +53,16 @@ btnEntrar.addEventListener("click", () => {
             );
 
             if (recuperacao === "senha123") {
-
-                alert(
-                    "Senha de recuperação aceita. " +
-                    "Você pode tentar fazer login novamente."
-                );
-
+                alert("Senha de recuperação aceita. Você pode tentar fazer login novamente.");
                 erro = 0;
                 btnEntrar.disabled = false;
-
             } else {
-
                 alert("Senha de recuperação incorreta.");
-
             }
         }
     }
+});
+
+btnCriarConta.addEventListener("click", () => {
+    window.location.href = "cadastro.html";
 });
